b3e7ed5a6a1bfb5adb57f4b7fab312c58dbbb66e
AndreaL <alafertteo@gmail.com>
2019-02-03 21:01:58 -0300

    Arreglado el orden de algunos componentes y rutas

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import * as M from "materialize-css/dist/js/materialize";
+
+// import { Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './logo/logo.component';
+import { ContentComponent } from './content/content.component';
+import { AppRoutingModule } from './app-routing.module';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    ContentComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+

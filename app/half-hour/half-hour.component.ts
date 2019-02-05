2e9ad733d5163bc923e380000818b2b7c26b2b37
AndreaL <alafertteo@gmail.com>
2019-02-05 10:42:42 -0300

    recuperado elemento random de array

--- app/half-hour/half-hour.component.ts
+++ app/half-hour/half-hour.component.ts
@@ -9,18 +9,18 @@ import { OmdbServiceClient } from '../services/omdb.service.client';
 export class HalfHourComponent implements OnInit {
   omdbData = ['0386676', '1865718', '0098904', '4508902', '0460649', '2861424', '0108778', '1305826', '0096697', '0149460'];
   randomItem = this.omdbData[Math.floor(Math.random() * this.omdbData.length - 1) + 1];
-  // seachData(randomItem);
-  id: String;
+  /*
+  id: String = this.randomItem;
   result: Object;
-  searchData(id: String) {
-    this.omdbService.searchDataById(id)
+  searchData() {
+    this.omdbService.searchDataById(this.id)
     .subscribe((result) => {
       this.result = result;
       console.log(result);
-    });
+    });*/
+  constructor(/*private omdbService: OmdbServiceClient*/) {
+    console.log(this.randomItem);
   }
-
-  constructor(private omdbService: OmdbServiceClient) { }
 
   ngOnInit() {
   }

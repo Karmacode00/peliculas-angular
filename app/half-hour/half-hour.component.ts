69b3bff89fb9c08c5e8821d2159d6129f801eeed
AndreaL <alafertteo@gmail.com>
2019-02-10 19:35:39 -0300

    todos los botones funcionando ok

--- app/half-hour/half-hour.component.ts
+++ app/half-hour/half-hour.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { OmdbServiceClient } from '../services/omdb.service.client';
 import { Http, Response } from '@angular/http';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-half-hour',
@@ -9,32 +10,32 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./half-hour.component.css']
 })
 export class HalfHourComponent implements OnInit {
-  omdbData = ['0386676', '1865718', '0098904', '4508902', '0460649', '2861424', '0108778', '1305826', '0096697', '0149460'];
-  randomItem = this.omdbData[Math.floor(Math.random() * this.omdbData.length - 1) + 1];
-  /*
-  id: String = this.randomItem;
-  result: Object;
-  searchData() {
-    this.omdbService.searchDataById(this.id)
-    .subscribe((result) => {
-      this.result = result;
-      console.log(result);
-    });*/
+
   constructor(private http: Http) {
-    // console.log(this.randomItem);
-    this.searchData();
   }
-  // id = this.randomItem;
-  searchData() {
-    this.http.get('https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?i=tt' + this.randomItem + '&apikey=a963a012')
-    .pipe(map(
-      (res: Response) => {
-        const result = res.json();
-        console.log(result);
-      }
-    ));
-    }
+  data: any [];
+  id;
+  getRandom() {
+    const omdbData = ['0386676', '1865718', '0098904', '4508902', '0460649', '2861424', '0108778', '1305826', '0096697', '0149460'];
+    const randomItem = omdbData[Math.floor(Math.random() * omdbData.length - 1) + 1];
+    return this.id =  randomItem;
+  }
+    again(): void {
+      window.location.reload();
+  }
   ngOnInit() {
-  }
+    this.data = [];
+
+    this.getRandom();
+    this.http
+        .get('https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?i=tt' + this.id + '&apikey=a963a012')
+        .pipe(map((res: Response) => res.json()))
+        .subscribe(data => {
+          this.data = Array.of(data);
+          console.log(data);
+          console.log(this.id);
+        });
+      }
+
 
 }
--- app/more-time/more-time.component.ts
+++ app/more-time/more-time.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-more-time',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoreTimeComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: Http) {
+  }
+  data: any [];
+  id;
+  getRandom() {
+    const omdbData = ['0111161', '0110912', '1375666', '0816692', '0317248', '0245429', '0119698', '0081505', '0169547', '3748528'];
+    const randomItem = omdbData[Math.floor(Math.random() * omdbData.length - 1) + 1];
+    return this.id =  randomItem;
+  }
+    again(): void {
+      window.location.reload();
+  }
   ngOnInit() {
+    this.data = [];
+
+    this.getRandom();
+    this.http
+        .get('https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?i=tt' + this.id + '&apikey=a963a012')
+        .pipe(map((res: Response) => res.json()))
+        .subscribe(data => {
+          this.data = Array.of(data);
+          console.log(data);
+          console.log(this.id);
+        });
   }
 
 }
+
+
